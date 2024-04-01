aeb55bf09a042cbaaf8082763c09d18841572474
CharanTM <charantm8787@gmail.com>
2024-04-01 23:58:42 +0530

    Bounted the code

--- fstrings.py
+++ fstrings.py
@@ -1,5 +1,5 @@
 name=input("Enter the name of the candidate:")
 weight=int(input("enter the weight in kg:"))
-height=int(input("Enter the height in meters:"))
+height=float(input("Enter the height in meters:"))
 bmi=weight/height**2
-print("I am ",name,"and my BMI is",round(bmi,2))
+print(f"I am {name} and my BMI is {bmi}")
