282e5d760aa9d5a2357636d5d9914c1c69fc2d9e
CharanTM <charantm8787@gmail.com>
2024-04-02 07:21:07 +0530

    Added a new code

--- conditions.py
+++ conditions.py
@@ -2,13 +2,20 @@ eligibility_criteria=int(input("Enter your current age:"))
 
 if (eligibility_criteria>=21 and eligibility_criteria<30):
     print("you are having a eligibile for marragie")
-    if (eligibility_criteria>=21 and eligibility_criteria<=24):
+    height=int(input(f"Enter your height in cm between your eligibility criteria:"))
+    if (eligibility_criteria>=21 and eligibility_criteria<=25 and height>=160 and height<170):
          print("and you get young and energitic girl to marry")
+    elif (eligibility_criteria>=26 and eligibility_criteria<=30 and height>=160 and height<=170):
+         print(" and you will get a normal young girl to marry")
+    elif (eligibility_criteria>=21 and eligibility_criteria<=25 and height>170):
+         print("you will get a young girl but she is more than your height") 
+    elif (eligibility_criteria>=26 and eligibility_criteria<=30 and height>170):
+         print(" and you will get a normal young girl to marry but she is more heighter than you")   
     else :
-         print(" and you will not get young girl to marry")     
-elif (eligibility_criteria>=60 and eligibility_criteria<=100):
+         print("you will marry but you will not get a young girl to marry")
+elif (eligibility_criteria>=60):
       print("you can not marry in this age")
-elif (eligibility_criteria>=30 and eligibility_criteria<40):
-       print(input("what type of age criteria is required:"))
+elif (eligibility_criteria>=30 and eligibility_criteria<60):
+       print("yep You will marry a girl little bit of older age but she also younger than you.")
 else :
     print("you are not eligible for marraige")
