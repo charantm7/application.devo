ac32c96e81fbf8269d11e2c00eb688fa6b2bf207
CharanTM <charantm8787@gmail.com>
2024-04-02 06:17:31 +0530

    comitted a conditions

---
+++ conditions.py
@@ -0,0 +1,8 @@
+eligibility_criteria=int(input("Enter your current age:"))
+
+if (eligibility_criteria>=21 and eligibility_criteria<60):
+    print("you are having a eligibile for marragie")
+if (eligibility_criteria>=60 and eligibility_criteria<=100):
+    print("you can not marry in this age")
+else :
+    print("you are not eligible for marraige")
