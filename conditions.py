98162c6783864d62774f10397e01679fb25f5a79
CharanTM <charantm8787@gmail.com>
2024-04-02 06:39:07 +0530

    commits a new code

--- conditions.py
+++ conditions.py
@@ -1,8 +1,14 @@
 eligibility_criteria=int(input("Enter your current age:"))
 
-if (eligibility_criteria>=21 and eligibility_criteria<60):
+if (eligibility_criteria>=21 and eligibility_criteria<30):
     print("you are having a eligibile for marragie")
-if (eligibility_criteria>=60 and eligibility_criteria<=100):
-    print("you can not marry in this age")
+    if (eligibility_criteria>=21 and eligibility_criteria<=24):
+         print("and you get young and energitic girl to marry")
+    else :
+         print(" and you will not get young girl to marry")     
+elif (eligibility_criteria>=60 and eligibility_criteria<=100):
+      print("you can not marry in this age")
+elif (eligibility_criteria>=30 and eligibility_criteria<40):
+       print(input("what type of age criteria is required:"))
 else :
     print("you are not eligible for marraige")
