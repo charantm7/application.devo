e39fbb62a705009c28b78bd5507a825ba40e36e4
CharanTM <charantm8787@gmail.com>
2024-04-03 06:03:26 +0530

    debugged

--- lab.py/lab3tutorial.py
+++ lab.py/lab3tutorial.py
@@ -1,19 +1,19 @@
-import math
+import math 
 def calculate_mean(numbers):
     return sum(numbers)/len(numbers)
 def calculate_variance(numbers):
-    mean= calculate_mean(numbers)
+    mean=calculate_mean(numbers)
     return sum((x-mean)**2 for x in numbers)/len(numbers)
 def calculate_standard_deviation(variance):
     return math.sqrt(variance)
-n=int(input("Enter the elements:"))
+n=int(input("enter the number of elements:"))
 numbers=0
 for i in range(n):
-    num=float(input(f"Enter the numbers {i+1}:"))
+    num=float(input(f"Enter the elements{i+1}:"))
 numbers.append(num)
 mean=calculate_mean(numbers)
 variance=calculate_variance(numbers)
 std_dev=calculate_standard_deviation(variance)
 print(f"Mean:{mean}")
 print(f"Variance:{variance}")
-print(f"Standard_deviation:{std_dev}")
+print(f"Standard Deviation:{std_dev}")
