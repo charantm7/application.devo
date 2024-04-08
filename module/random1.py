cde465e397c13893b674d7c82936ea3aca979b03
CharanTM <charantm8787@gmail.com>
2024-04-08 22:51:34 +0530

    added module to python exercise

--- module/random1.py
+++
@@ -1,5 +0,0 @@
-import random
-name=input("Enter the everybody's name:").split(',')
-a=random.shuffle(name)
-b=name[0]
-print(b)
--- module/random2.py
+++
@@ -1,5 +0,0 @@
-import random
-name=input("Enter everybody's name separated by commas:")
-a=name.split(',')
-b=random.choice(a)
-print(f"{b} will pay the bill")
