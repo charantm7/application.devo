7c6168311c045e7750c64fe74f45971412eb4765
CharanTM <charantm8787@gmail.com>
2024-04-16 23:38:05 +0530

    added new code

---
+++ lab.py/lab4.py
@@ -0,0 +1,12 @@
+def count_digit_frequency(number):
+    digit_frequency={}
+    for digit in number:
+        if digit.isdigit():
+            if digit in digit_frequency:
+                digit_frequency[digit]+=1
+            else:
+                digit_frequency[digit]=1
+    for digit, frequency in digit_frequency.items():
+        print(f"Frequency of digit {digit}:{frequency}")
+number=input("Enter the muilti-digit number:")
+count_digit_frequency(number)
