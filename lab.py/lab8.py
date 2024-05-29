b71dbdc31723729ac4e74ae5c1bad71d92f80d88
CharanTM <charantm8787@gmail.com>
2024-05-29 07:23:34 +0530

    .

--- lab.py/lab8.py
+++ lab.py/lab8.py
@@ -1,19 +1,19 @@
 import sys
 
 def DivExp(a,b):
-    assert a>0 , "a should be greater than 0"
+    assert a>0, "a should be greater than 0"
 
     try:
-        c = a/b
+        c=a/b
 
     except ZeroDivisionError:
-        print("b canot be zero")
-        sys.exit(0)
+        print("B must not be zero")
 
     else:
         return c
     
-val1 = int(input("Enter the value for a: "))
-val2 = int(input("Enter the value for b: "))
+val1 = int(input("Enter the value of a: "))
+val2 = int(input("Enter the value of b: "))
 val3 = DivExp(val1,val2)
+
 print(f"{val1}/{val2}={val3}")
